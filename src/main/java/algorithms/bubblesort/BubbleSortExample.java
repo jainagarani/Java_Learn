9228c46d6bfbf4f29309bf6e8386f5faf73daee3
jainagarani <jainagarani@gmail.com>
2025-05-06 18:11:25 +0530

    Jai learning java

--- src/main/java/algorithms/bubblesort/BubbleSortExample.java
+++ src/main/java/algorithms/bubblesort/BubbleSortExample.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class BubbleSortExample {
     public static void main(String[] args){
-        int[] arr = {5,6,4,8,9};
+      /*  int[] arr = {5,6,4,8,9};
         for(int i =0; i < arr.length-1; i++){
           for(int j =i+1; j<arr.length;j++){
             if(arr[i] >arr[j]){
@@ -17,6 +17,17 @@ public class BubbleSortExample {
           }
 
         }
-        Arrays.stream(arr).forEach(System.out::print);
+        Arrays.stream(arr).forEach(System.out::print);*/
+        int[] arr = {10,3,5,6};
+        for(int i=0; i< arr.length; i++){
+            for(int j =i+1; j< arr.length; j++){
+                if(arr[i] > arr[j]){
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                 }
+            }
+        }
+        Arrays.stream(arr).forEach(System.out::println);
     }
 }
